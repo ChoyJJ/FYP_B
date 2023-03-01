b747d416cf32af7a366673b03fbdf78450ededa1
Choyjj <jet000905@gmail.com>
2023-03-02 00:05:25 +0800

    Update on module file for CM and AUC

--- Training Models/module.py
+++ Training Models/module.py
@@ -3,9 +3,11 @@ import csv
 import os
 import numpy as np
 import shutil
+import matplotlib.pyplot as plt
+from sklearn.metrics import roc_curve, auc, roc_auc_score
 
 class Model_Training:
-    def pretrained_network(name='inceptionv3',img_height=300,img_width=300):
+    def pretrained_network(self,name='inceptionv3',img_height=300,img_width=300):
         """
         Insert the name of the pretrained model in small letters without spacing, the function will return the preprocessing layer and the model layer pre-trained on imagenet.
         """
@@ -33,7 +35,7 @@ class Model_Training:
             pre_trained = tf.keras.applications.inception_v3.InceptionV3(weights='imagenet', include_top=False, input_shape=(img_height,img_width,3))   
         return preprocessing, pre_trained
     
-    def training(model,train_dataset,validation_dataset,total_epochs,save_weights,patience,Earlystop,train_log,callbacks,optimiser):
+    def training(self,model,train_dataset,validation_dataset,total_epochs,save_weights,patience,Earlystop,train_log,callbacks,optimiser):
 
         best_weights = None
         best_val_loss = float('inf')
@@ -107,17 +109,17 @@ class Model_Training:
                     break
         return model, all_history
 
-    def build_model(pretrained_model,trainable_layers=False,augmentation=True,Flatten='flatten',regulariser=False,load_weights = False,img_height=300,img_width=300,
+    def build_model(self,pretrained_model,trainable_layers=False,augmentation=True,Flatten='flatten',regulariser=None,load_weights = False,img_height=300,img_width=300,
                     optimiser=tf.keras.optimizers.Adam(),
                     losses = tf.keras.losses.CategoricalCrossentropy(),
                     metrics = [tf.keras.metrics.CategoricalAccuracy(),tf.keras.metrics.Recall(class_id=0),tf.keras.metrics.Recall(class_id=1)]):
         data_augmentation = tf.keras.Sequential([
           tf.keras.layers.RandomFlip('horizontal_and_vertical'),
-          tf.keras.layers.RandomRotation((-0.2,0.2),fill_mode='nearest'),
-          tf.keras.layers.RandomZoom(height_factor=(-0.2,0.7),width_factor=(-0.2,0.7),fill_mode='nearest'),
-          tf.keras.layers.RandomTranslation(height_factor=(-0.1,0.1),width_factor=(-0.1,0.1),fill_mode='nearest')
+          tf.keras.layers.RandomRotation((-0.2,0.2),fill_mode='constant'),
+          tf.keras.layers.RandomZoom(height_factor=(-0.5,0.5),width_factor=(-0.5,0.5),fill_mode='constant'),
+          tf.keras.layers.RandomTranslation(height_factor=(-0.2,0.2),width_factor=(-0.2,0.2),fill_mode='constant')
           ])
-        preprocessing,pre_trained = Model_Training.pretrained_network(name=pretrained_model,img_height=img_height,img_width=img_width)
+        preprocessing,pre_trained = self.pretrained_network(name=pretrained_model,img_height=img_height,img_width=img_width)
         if trainable_layers:
             pre_trained.trainable = True
             for layer in pre_trained.layers:
@@ -129,7 +131,7 @@ class Model_Training:
             pre_trained.trainable = False
         tfinput = tf.keras.layers.Input(shape=(img_height,img_width,3))
         if augmentation:
-            data_augment = data_augmentation(tfinput)
+            data_augment = data_augmentation(tfinput,training=True)
             pre_process = preprocessing(data_augment)
         else:
             pre_process = preprocessing(tfinput)
@@ -139,20 +141,20 @@ class Model_Training:
             x = tf.keras.layers.Dense(128,activation = 'relu')(flatten)
         elif Flatten == 'global_average_pooling':
             flatten = tf.keras.layers.GlobalAveragePooling2D()(tl_model)
-            x = tf.keras.layers.Dense(2048,activation = 'relu')(flatten)
+            x = tf.keras.layers.Dense(4096,activation = 'relu')(flatten)
         elif Flatten == 'global_max_pooling':
             flatten = tf.keras.layers.GlobalMaxPooling2D()(tl_model)
             x = tf.keras.layers.Dense(2048,activation = 'relu')(flatten)
         x = tf.keras.layers.Dense(1024,activation='relu')(x)
-        x= tf.keras.layers.Dropout(0.5)(x)
-        if regulariser:
+        x= tf.keras.layers.Dropout(0.7)(x)
+        if regulariser != None:
             x = tf.keras.layers.Dense(8,activation='relu',kernel_regularizer=regulariser)(x)
         else:
             x = tf.keras.layers.Dense(8,activation='relu')(x)
         output = tf.keras.layers.Dense(2, activation="softmax")(x)
         model = tf.keras.models.Model(tfinput,output)
         model.summary()
-        if load_weights:
+        if load_weights != False:
             model.load_weights(load_weights)
         model.compile(
                         optimizer= optimiser,
@@ -161,7 +163,7 @@ class Model_Training:
                     ) 
         return model
     
-    def main(           pretrained_model,
+    def main(           self,pretrained_model,
                         train_dataset,
                         validation_dataset,
                         epochs,
@@ -183,19 +185,19 @@ class Model_Training:
         """
         pretrained_model is the name of the CNN model used in this transfer learning method
         """
-        model = Model_Training.build_model(pretrained_model,trainable_layers,augmentation,flatten,regulariser,load_weights,img_height,img_width,optimiser,losses,metrics)
+        model = self.build_model(pretrained_model,trainable_layers,augmentation,flatten,regulariser,load_weights,img_height,img_width,optimiser,losses,metrics)
         optimiser.lr = learning_rate
         
         
         
         
         
-        model, history = Model_Training.training(model,train_dataset,validation_dataset,epochs,save_weights,patience,Earlystop,train_log,callbacks,optimiser)
+        model, history = self.training(model,train_dataset,validation_dataset,epochs,save_weights,patience,Earlystop,train_log,callbacks,optimiser)
 
 
         return model, history
     
-    def misclassified_images(   model,test_dataset,load_weights=False,
+    def misclassified_images(   self,model,test_dataset,load_weights=False,
                                 optimiser=tf.keras.optimizers.Adam(),
                                 losses = tf.keras.losses.CategoricalCrossentropy(),
                                 metrics = [tf.keras.metrics.CategoricalAccuracy(),tf.keras.metrics.Recall(class_id=0),tf.keras.metrics.Recall(class_id=1)]
@@ -226,12 +228,12 @@ class Model_Training:
 
         return misclassified_images
     
-    def store_misclassified(model,test_dataset,dst,load_weights=False,
+    def store_misclassified(self,model,test_dataset,dst,load_weights=False,
                             optimiser=tf.keras.optimizers.Adam(),
                             losses = tf.keras.losses.CategoricalCrossentropy(),
                             metrics = [tf.keras.metrics.CategoricalAccuracy(),tf.keras.metrics.Recall(class_id=0),tf.keras.metrics.Recall(class_id=1)]
                             ):
-        misclassified = Model_Training.misclassified_images( model,test_dataset,load_weights,optimiser,losses,metrics)
+        misclassified = self.misclassified_images( model,test_dataset,load_weights,optimiser,losses,metrics)
         dest_dir_path = dst
         os.makedirs(dest_dir_path, exist_ok=True)
         os.makedirs(dest_dir_path+'benign/', exist_ok=True)
@@ -250,9 +252,109 @@ class Model_Training:
         return misclassified
     
     
+    def datasets(self,data_loc,label_mode='categorical',img_height: int=300,img_width: int=300, batch_size: int=300):
+        return tf.keras.utils.image_dataset_from_directory(data_loc,
+                                                            label_mode=label_mode,
+                                                            image_size=(img_height, img_width),
+                                                            batch_size=batch_size)
     
+    def confusion_matrix(self,test_data,predictions,plot=True):
+        '''
+        Directly import the test dataset used for model prediction, and insert the prediction from model.predict()
+        Returns: Confusion Metrics as a 2x2 array
+        '''
+        labels = []
+        for x,y in test_data.unbatch():
+            labels.append(y.numpy().astype('uint8'))
+        onehot = np.argmax(predictions,axis=1)
+        pred = np.zeros((len(onehot),2),dtype=int)
+        for i,x in enumerate(onehot):
+            pred[i,x]=1
+        confusion_matrix = np.zeros((2,2),dtype=int)
+        for i,x in enumerate(labels):
+            if x[0] == 1:
+                if pred[i,0] == 1:
+                    confusion_matrix[0,0]+=1
+                else:
+                    confusion_matrix[0,1]+=1
+            elif x[1] ==1:
+                if pred[i,1] == 1:
+                    confusion_matrix[1,1]+=1
+                else:
+                    confusion_matrix[1,0]+=1
+        if plot:
+            classes = ['Benign', 'Malignant']
+            fig, ax = plt.subplots(figsize=(8, 8))
+            im = ax.imshow(confusion_matrix, cmap='Blues')
+
+            # Add axis labels and tick marks
+            ax.set_xticks(np.arange(len(classes)))
+            ax.set_yticks(np.arange(len(classes)))
+            ax.set_xticklabels(classes)
+            ax.set_yticklabels(classes)
+
+            # Add annotations
+            thresh = confusion_matrix.max() / 2
+            for i in range(len(classes)):
+                for j in range(len(classes)):
+                    ax.text(j, i, format(confusion_matrix[i, j], 'd'),
+                            ha='center', va='center',
+                            color='white' if confusion_matrix[i, j] > thresh else 'black')
+
+            ax.set_title('Confusion Matrix')
+            fig.colorbar(im)
+
+            # Display the plot
+            plt.show()
+        return confusion_matrix
     
-    
+    def AUC(self,test,prediction):
+        '''
+        Directly import the test dataset used for model prediction, and insert the prediction from model.predict()
+        Returns: AUC score 
+        '''
+        y_true = np.zeros((2,))
+        for x,y in test.unbatch():
+            y_true = np.vstack((y_true,y.numpy()))
+        y_true = y_true[1:]
+        y_true.astype('uint8')
+        y_pred = prediction
+        # y_true and y_pred are numpy arrays containing the true labels and predicted probabilities for each class, respectively
+        n_classes = y_true.shape[1]
+        fpr = dict()
+        tpr = dict()
+        roc_auc = dict()
+        for i in range(y_true.shape[1]):
+            fpr[i], tpr[i], _ = roc_curve(y_true[:, i], y_pred[:, i])
+            roc_auc[i] = auc(fpr[i], tpr[i])
+
+        # Compute micro-average ROC curve and AUC
+        fpr["micro"], tpr["micro"], _ = roc_curve(y_true.ravel(), y_pred.ravel())
+        roc_auc["micro"] = auc(fpr["micro"], tpr["micro"])
+
+        # Plot ROC curves for each class and micro-average
+        plt.figure()
+        lw = 2
+        plt.plot(fpr["micro"], tpr["micro"], color='darkorange',
+                 lw=lw, label='micro-average ROC curve (area = {0:0.2f})'
+                 ''.format(roc_auc["micro"]))
+        for i in range(y_true.shape[1]):
+            plt.plot(fpr[i], tpr[i], lw=lw,
+                     label='ROC curve of class {0} (area = {1:0.2f})'
+                     ''.format(i, roc_auc[i]))
+
+        plt.plot([0, 1], [0, 1], color='navy', lw=lw, linestyle='--')
+        plt.xlim([0.0, 1.0])
+        plt.ylim([0.0, 1.05])
+        plt.xlabel('False Positive Rate')
+        plt.ylabel('True Positive Rate')
+        plt.title('Receiver operating characteristic')
+        plt.legend(loc="lower right")
+        plt.show()
+        auc_score = roc_auc_score(y_true, prediction)
+
+        return auc_score
+
     
     
     
